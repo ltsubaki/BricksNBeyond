6d5151ceb3956595c4b79f5410e49f8c42c478c9
liliantsubaki <liliantsubaki@gmail.com>
2024-04-04 18:19:51 -0600

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace IntexQueensSlay.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
