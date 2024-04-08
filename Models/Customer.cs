66ee4f5eb5eda26d2505c7f023ce67cb31b2a3e6
liliantsubaki <liliantsubaki@gmail.com>
2024-04-08 16:09:04 -0600

    Added data classes!

--- Models/Customer.cs
+++ Models/Customer.cs
@@ -11,5 +11,8 @@ namespace IntexQueensSlay.Models
         public string birthDate { get; set; }
         public string resCountry { get; set; }
         public string gender { get; set; }
+        private string username { get; set; }
+        private string password { get; set; }
+        public bool admin { get; set; }
     }
 }
---
+++ Models/LineItem.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace IntexQueensSlay.Models
+{
+    public class LineItem
+    {
+        [Key]
+        [Column(Order = 1)] // This sets the order of composite keys (optional)
+        public int transactionId { get; set; }
+
+        [Key]
+        [Column(Order = 2)] // This sets the order of composite keys (optional)
+        public int productId { get; set; }
+        public int quantity { get; set; }
+        public int rating { get; set; }
+    }
+}
