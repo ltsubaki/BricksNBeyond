ad748eb8ddde4bec3318679fee529530ad3fbd18
Amy Fox <amyfox723@gmail.com>
2024-04-09 13:23:53 -0600

    clean up clean up

---
+++ Components/CategoryMenuViewComponent.cs
@@ -0,0 +1,25 @@
+﻿using IntexQueensSlay.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IntexQueensSlay.Components
+{
+    public class CategoryMenuViewComponent : ViewComponent 
+    {
+        private ISlayRepository _repo;
+
+        public CategoryMenuViewComponent(ISlayRepository temp)
+        {
+            _repo = temp;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var categoryMenu = _repo.Products
+                .Select(x => x.Category1)
+                .Distinct()
+                .OrderBy(x => x);
+
+            return View(categoryMenu);
+    }   
+    }
+}
