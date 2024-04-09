223664448def45c3411abbef9fac0ee21233975f
bookworm2035 <bookworm2035@gmail.com>
2024-04-08 20:19:21 -0600

    Add repo pattern

---
+++ Models/EFSlayRepository.cs
@@ -0,0 +1,18 @@
+﻿namespace IntexQueensSlay.Models
+{
+    public class EFSlayRepository: ISlayRepository
+    {
+        private LegoContext _context;
+
+        public EFSlayRepository(LegoContext temp) 
+        {
+            _context = temp;
+        }
+
+        public IQueryable<Customer> Customers => _context.Customers;
+        public IQueryable<Product> Products => _context.Products;
+        public IQueryable<LineItem> LineItems => _context.LineItems;
+        public IQueryable<Order> Orders => _context.Orders;
+
+    }
+}
