095b80ba629f7c1f77c026acadecea739e89d58f
Amy Fox <amyfox723@gmail.com>
2024-04-12 00:11:02 -0600

    idek

--- Models/EFSlayRepository.cs
+++ Models/EFSlayRepository.cs
@@ -66,7 +66,7 @@ namespace IntexQueensSlay.Models
 
         public void AddCustomer(Customers task)
         {
-            _context.Add(task);
+            _context.Customers.Add(task);
             _context.SaveChanges();
         }
 
